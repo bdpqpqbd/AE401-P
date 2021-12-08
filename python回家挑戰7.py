e8e52854bfde1567d45c1696312343551dde4b25
bdpqpqbd <93990601+bdpqpqbd@users.noreply.github.com>
2021-12-08 21:09:29 +0800

    Add files via upload

---
+++ python回家挑戰7.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 18:59:33 2021
+
+@author: Jay
+"""
+
+
+name = ["a man","a woman","a dog","a cat"]
+v = ["watches","holds","hits"]
+n = ["a phone.","a toilet.","a frog."]
+import random
+a = random.sample(name, 1)
+b = random.sample(v, 1)
+c = random.sample(n, 1)
+print(a,b,c)
